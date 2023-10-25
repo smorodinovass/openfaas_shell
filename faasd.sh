21fa0ffd928de0c290b65323608684f5ee959666
smorodinovass <81092012+smorodinovass@users.noreply.github.com>
2023-10-25 22:30:42 +0300

    Add files via upload

---
+++ faasd.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+apt-get install -y
+cd /tmp
+git clone https://github.com/openfaas/faasd --depth=1 #install faasd
+cd faasd
+./hack/install.sh
+curl -sSL https://cli.openfaas.com | sudo sh #intall faas-cli
+sudo cat /var/lib/faasd/secrets/basic-auth-password #password for faas gateway-panel
+echo
