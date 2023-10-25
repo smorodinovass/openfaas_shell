13457cbced23331af4040a31c7fa5aba53325b1f
smorodinovass <81092012+smorodinovass@users.noreply.github.com>
2023-10-25 22:13:01 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,7 +3,7 @@ apt-get install -y
 cd /tmp
 git clone https://github.com/openfaas/faasd #install faasd
 cd faasd
-./hack/install.sh
+bash ./hack/install.sh
 curl -sSL https://cli.openfaas.com | sudo sh #intall faas-cli
 sudo cat /var/lib/faasd/secrets/basic-auth-password #password for faas gateway-panel
 echo
